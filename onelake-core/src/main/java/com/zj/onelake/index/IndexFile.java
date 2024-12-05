b511cbb0a57c9decd665c8fbc7d0659b2a0a7bbb
zlzhang0122 <zlzhang0122@gmail.com>
2024-12-05 23:34:44 +0800

    [commit]Add commit info

---
+++ onelake-core/src/main/java/com/zj/onelake/index/IndexFile.java
@@ -0,0 +1,11 @@
+package com.zj.onelake.index;
+
+/**
+ * @Author geniuszhang
+ * @Date 2024/12/5 23:33
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class IndexFile {
+
+}
--- onelake-core/src/main/java/com/zj/onelake/write/Committable.java
+++ onelake-core/src/main/java/com/zj/onelake/write/Committable.java
@@ -1,5 +1,10 @@
 package com.zj.onelake.write;
 
+import com.zj.onelake.index.IndexFile;
+import org.apache.flink.table.data.binary.BinaryRowData;
+
+import java.util.List;
+
 /**
  * @Author geniuszhang
  * @Date 2024/12/1 21:31
@@ -7,5 +12,41 @@ package com.zj.onelake.write;
  * @Version 1.0
  */
 public class Committable {
+    private final long commitId;
 
+    private final BinaryRowData partition;
+
+    private final int bucket;
+
+    // Newly created data files.
+    protected final List<DataFile> newDataFiles;
+
+    // Files changed before.
+    protected final List<DataFile> compactBefore;
+
+    // Files after compaction.
+    protected final List<DataFile> compactAfter;
+
+    protected final List<DataFile> changelogFiles;
+
+    protected final List<IndexFile> indexFiles;
+
+    public Committable(
+            long commitId,
+            BinaryRowData partition,
+            int bucket,
+            List<DataFile> newDataFiles,
+            List<DataFile> compactBefore,
+            List<DataFile> compactAfter,
+            List<DataFile> changelogFiles,
+            List<IndexFile> indexFiles) {
+        this.commitId = commitId;
+        this.partition = partition;
+        this.bucket = bucket;
+        this.newDataFiles = newDataFiles;
+        this.compactBefore = compactBefore;
+        this.compactAfter = compactAfter;
+        this.changelogFiles = changelogFiles;
+        this.indexFiles = indexFiles;
+    }
 }
